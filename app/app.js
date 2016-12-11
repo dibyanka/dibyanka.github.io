9bd403f2b89f72c4ed685e5a1f071cbf8c649cac
dib <dibyanka.2401@gmail.com>
2016-12-11 20:28:02 +0700

    layout change

--- app/app.js
+++ app/app.js
@@ -10,3 +10,32 @@ portfolioApp.controller('mainController', function ($scope) {
                         
 });
 
+portfolioApp.run(['$anchorScroll', function ($anchorScroll) 
+    {
+        $anchorScroll.yOffset = 50;   // always scroll by 50 extra pixels                     
+    } ])
+    .controller('scrollController', [ '$scope', '$location', '$anchorScroll',
+                                            function($scope, $location, $anchorScroll)
+                                            {
+                                            $scope.goToSection=function(x) {
+                                                var newHash= x;
+                                                
+                                                if($location.hash() !== newHash)
+                                                    {
+                        // set the $location.hash to `newHash` and
+                        // $anchorScroll will automatically scroll to it
+                                                $location.hash( x);                                                          
+                                                        
+                                                    }
+                                                else
+                                                    {
+                        // call $anchorScroll() explicitly,
+                        // since $location.hash hasn't changed
+                                                $anchorScroll();                                                          
+                                                    }                                                
+                                            };    
+                                                
+                                            }
+                                            
+                                            ]);
+
