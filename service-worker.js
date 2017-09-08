5e6e7705f6f48590ec86f31ebc8ac37d6c369e63
dib <dibyanka.2401@gmail.com>
2017-09-07 19:19:29 -0700

    Implemented push notifications as PWA extension.

--- service-worker.js
+++ service-worker.js
@@ -46,3 +46,64 @@ self.addEventListener('fetch', function(event) {
   );
 });
 
+
+/* eslint-disable max-len */
+
+const applicationServerPublicKey = 'BGa5aFaTOtvKhNre55AII7qUtWaOPD6EZlWnon544RZgZBGpO4WV-E--Et-xmiKDVwHs51uUYwd5YFBP0Qkcyaw';
+
+/* eslint-enable max-len */
+
+function urlB64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'Push Notification';
+  const options = {
+    body: 'Yay it works.',
+    icon: 'content/images/pushNotification/icon.png',
+    badge: 'content/images/pushNotification/badge.png'
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
+
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow('https://adichourasiya.github.io')
+  );
+});
+
+
+self.addEventListener('pushsubscriptionchange', function(event) {
+  console.log('[Service Worker]: \'pushsubscriptionchange\' event fired.');
+  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+  event.waitUntil(
+    self.registration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: applicationServerKey
+    })
+    .then(function(newSubscription) {
+      // TODO: Send to application server
+      console.log('[Service Worker] New subscription: ', newSubscription);
+    })
+  );
+});
